d966928d654dbca99f1dac270f32aa6412f6b385
John Oseni <johnoseni26@gmail.com>
2022-01-17 23:08:40 +0100

    Wrote the .bat for the Pl

--- diamond.bat
+++ diamond.bat
@@ -3,3 +3,8 @@
 @REM This contains contains various commands used for
 @REM repetitive tasks or to run the groups of scripts one after another.
 
+    @echo off
+
+    @REM To execute the main python file for the whole diamond-programing language
+    
+    python diamond-execute.py %*
