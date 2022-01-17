a3d1a65699dd28592f17d760103529e9a80a9634
John Oseni <johnoseni26@gmail.com>
2022-01-17 23:59:22 +0100

    fixed up the bug here

--- rundiamond.py
+++ rundiamond.py
@@ -13,7 +13,7 @@ def run_function(index, python_funcs_and_vars, lines, line, _functions, variable
         index,
         lines,
         _functions,
-        python_funcs_and_vars
+        python_funcs_and_var
     )
 
 def run_main(line, variables, index, lines, _functions_dict, python_funcs_and_vars):
