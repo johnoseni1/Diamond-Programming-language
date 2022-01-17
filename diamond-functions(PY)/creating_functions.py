7e431b256c24676299280e4c49f6830746d9957c
John Oseni <johnoseni26@gmail.com>
2022-01-18 00:49:07 +0100

    the real languages

---
+++ diamond-functions(PY)/creating_functions.py
@@ -0,0 +1,19 @@
+
+# syntax for creating functions
+
+
+from . import syntax_error
+
+def create_function(function_name, current_lines):
+    function_lines = []
+
+    if not function_name.endswith(':'):
+        syntax_error()
+
+    function_name = function_name[:len(function_name) - 1]
+
+    for current_line in current_lines:
+        if current_line.startswith('    '):
+            function_lines.append(current_line.replace('',''))
+
+    functions[function_name] = function_lines
